3591ae85a8f23ad3242007aff4e731083a089b48
WesclaySilva <wesclayts@gmail.com>
2026-07-01 16:26:50 -0300

    adiciona if else

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 
-let resultado = vitoriasEDerrotas(10, 5);
+let resultado = nivelHeroi(101);
+let saldo = saldoVitorias(101, 50);
+console.log(resultado);
 
-function vitoriasEDerrotas(vitorias, derrotas) {
-    
+function nivelHeroi(vitorias) {
+   let nivel;
+    if (vitorias < 10) {
+        nivel = 'Ferro';
+    } else if (vitorias < 21) {         
+        nivel = 'Bronze';
+    } else if (vitorias < 51) {
+        nivel = 'Prata';
+    } else if (vitorias < 81) {
+        nivel = 'Ouro';
+    } else if (vitorias < 91) {
+        nivel = 'Diamante';
+    } else if (vitorias < 101) {
+        nivel = 'Lendário';
+    } else {
+        nivel = 'Imortal';
+    return nivel;
+    }
+}
+
+function saldoVitorias(vitorias, derrotas) {
+    return vitorias - derrotas;
 }
